972f49d4b705ae54296c3bc98c56f3748bd83f84
Obaidullah-ahsan <abir60303@gmail.com>
2024-06-04 20:24:44 +0600

    when user click heart icon trip added to the wishlist

--- src/Components/Shared/PackageCard.jsx
+++ src/Components/Shared/PackageCard.jsx
@@ -1,11 +1,35 @@
 import { Link } from "react-router-dom";
+import { FaRegHeart } from "react-icons/fa";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import toast from "react-hot-toast";
 
 const PackageCard = ({ pac }) => {
   const { _id, image, tour_type, tour_title, price } = pac;
+  const axiosPublic = useAxiosPublic();
+  const handleWishlist = () => {
+    const wishlist = {
+      packageId: _id,
+      tour_title,
+      tour_type,
+      price,
+    };
+    axiosPublic.post("/wishlist", wishlist).then((res) => {
+      console.log(res.data);
+      if (res.data.insertedId) {
+        toast.success("Trip added to the wishlist");
+      }
+    });
+  };
   return (
     <div className="card card-compact rounded-md bg-base-100 hover:shadow-xl p-3">
       <figure>
-        <img className="max-h-40 w-full" src={image} alt="Shoes" />
+        <img className="max-h-40 w-full relative" src={image} alt="Shoes" />
+        <button
+          onClick={handleWishlist}
+          className="rounded-full absolute right-6 top-6 p-2 bg-slate-100"
+        >
+          <FaRegHeart color="#FF9051" size={20} />
+        </button>
       </figure>
       <div className="p-1">
         <p className="text-gray-500">{tour_type}</p>
